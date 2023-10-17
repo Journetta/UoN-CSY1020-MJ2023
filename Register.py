2eaee1c1c48ee88b2bcce42d5f3b2a6db214f7ca
Maisy <71196440+Journetta@users.noreply.github.com>
2023-10-17 15:39:37 +0100

    Update Register.py

--- Register.py
+++ Register.py
@@ -1,9 +1,14 @@
-register = ["Maisy", "Aimee", "Catherine", "Shane", "Lily", "Nelly", "Frances", "Aaron", "Ethan"]
+register = [ "Maisy", "Catherine", "Aimee", "Shane", "Lily", "Nelly", "Frances", "Aaron", "Ethan"]
 found = input("What is the name you want to find in the register? ")
-num = 1
+num = 0
 
 for x in register:
-        print ( str(num) + ": " + x)
-        num = num+1
-        if x==found:
-                 break
+    num = num + 1
+    print ( str(num) + ": " + x)
+    if x==found:
+            print(found + "'s Student Number is: " + str(num))
+            break
+
+if x !=found:
+    
+    print ("Student could Not be found")
